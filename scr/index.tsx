ad80e20148df315126d8356af7f3e6736cab428d
yurlovakristina <yurlovakristina9@gmail.com>
2025-04-05 00:16:01 +0200

    Add files via upload

---
+++ scr/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// 🔥 Удалили reportWebVitals — он больше не нужен!
